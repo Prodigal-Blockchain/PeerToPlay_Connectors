5b07b7e2b5611c9d2b9b3bc7b019a4a50de8614c
Pratima-M1 <144767684+Pratima-M1@users.noreply.github.com>
2024-05-25 12:52:30 +0530

    Update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+const url = process.env.RPC_URL;
+const wallet = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: url,
+      accounts: [wallet],
+      chainId: 11155111,
+      maxFeePerGas: 100000000,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
   solidity: {
     compilers: [
       {
-        version: "0.8.0",
+        version: "0.8.20",
         settings: {
           optimizer: { enabled: true },
         },
@@ -23,6 +42,15 @@ module.exports = {
           optimizer: { enabled: true },
         },
       },
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: { enabled: true },
+        },
+      },
     ],
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
   },
 };
